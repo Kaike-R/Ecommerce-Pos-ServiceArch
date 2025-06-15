f51c9b0f43f2ea36daea2e65abd5595a3839ab2e
Kaike <kzuanetti53@gmail.com>
2025-06-15 03:40:02 -0300

    Alteracoes no Dockerfile para adicionar o Sonarqube

--- UserService/src/main/java/br/com/kaikedev/userservice/Controller/UserController.java
+++ UserService/src/main/java/br/com/kaikedev/userservice/Controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
